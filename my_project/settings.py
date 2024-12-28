79d7feee20800f890c44b251caa805aed2821e9d
Irelandoracle1 <nairajnr1@gmail.com>
2024-12-28 02:01:05 +0000

    included setting to host app on heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dhhfw0s&c2in)n_gs469k7ek!n2ipq^1bvcnfr%en@^sb3x0%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
